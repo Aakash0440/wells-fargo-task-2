ef5427438fec714f866ae5d027eb8803b7d12210
Aakash0440 <Aakashali.03002985610@gmail.com>
2026-01-11 12:22:51 +0500

    Initial setup of the project structure and configuration files.

---
+++ src/main/java/com/wellsfargo/counselor/entity/FinancialAdvisor.java
@@ -0,0 +1,69 @@
+package com.wellsfargo.counselor.entity;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+public class FinancialAdvisor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    private String phone;
+
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
+
+    public FinancialAdvisor(Long advisorId, String name, String email, String phone, List<Client> clients) {
+        this.advisorId = advisorId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.clients = clients;
+    }
+
+    public FinancialAdvisor() {}
+
+    public Long getAdvisorId() {
+        return advisorId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+ 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+ 
+    public String getPhone() {
+        return phone;
+    }
+ 
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+ 
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+}
